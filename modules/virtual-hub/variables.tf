1930d9559e31af3656b4f5cee4cf730e3e818dac
Mudassar Sarfraz <sahil_k_mudassar@yahoo.com>
2026-04-20 15:10:47 +0100

    yes

--- modules/virtual-hub/variables.tf
+++ modules/virtual-hub/variables.tf
@@ -3,7 +3,7 @@ variable "location" { type = string }
 variable "resource_group_name" { type = string }
 variable "virtual_wan_id" { type = string }
 variable "address_prefix" { type = string }
-variable "tags" { 
-  type = map(string) 
-  default = {} 
+variable "tags" {
+  type    = map(string)
+  default = {}
 }
